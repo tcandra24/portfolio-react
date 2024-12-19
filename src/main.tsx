176db1f6c757a4ebe243c6060bbc4e7f1f2e1241
tcandra24 <tcandra007@gmail.com>
2024-12-19 11:38:58 +0700

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/css/main.css";
+
+// Router
+import { RouterProvider } from "react-router-dom";
+import router from "./router/index";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store/index";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <Suspense>
+        <RouterProvider router={router} />
+      </Suspense>
+    </Provider>
+  </StrictMode>
+);
---
+++ src/router/routes.tsx
@@ -0,0 +1,11 @@
+import { lazy } from "react";
+const Index = lazy(() => import("../pages/homepage/Index"));
+
+const routes = [
+  {
+    path: "/",
+    element: <Index />,
+  },
+];
+
+export { routes };
